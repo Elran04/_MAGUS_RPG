f307d15e2c8f3bea1d8b28f1ee5d6797a29b0d8c
Richárd Jeremcsuk <jeremcsukrichard@gmail.com>
2025-10-29 13:44:16 +0100

    Class editor modularised and first refactoring steps made

---
+++ Gamemaster_tools/ui/classes/class_editor_constants.py
@@ -0,0 +1,13 @@
+"""
+Class Editor Constants
+Contains all constant values used in the class editor
+"""
+
+# Equipment item types
+EQUIPMENT_TYPES = ["armor", "weaponandshield", "general"]
+
+# Stat names (order matches database schema)
+STAT_NAMES = [
+    "Erő", "Állóképesség", "Gyorsaság", "Ügyesség", "Karizma",
+    "Egészség", "Intelligencia", "Akaraterő", "Asztrál", "Érzékelés"
+]
